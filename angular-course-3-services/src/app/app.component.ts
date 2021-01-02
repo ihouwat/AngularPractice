435800134eef263319a7e103183b137f7c3daf28
Igor <houwat@gmail.com>
2021-01-02 14:42:21 -0500

    finished services section

--- angular-course-3-services/src/app/app.component.ts
+++ angular-course-3-services/src/app/app.component.ts
@@ -4,6 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {CoursesService} from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +14,40 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$ : Observable<Course[]> // Observable variable
 
-  courses = COURSES;
+  // courses;
 
-  constructor() {
+  constructor(private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
+
+    // // Setting HTTP parameters
+    // const params = new HttpParams()
+    //   .set("page", "1")
+    //   .set("pageSize", "10");
+
+    // FIRST METHOD:  GET request - manual subscription method
+    // this.http.get('/api/courses', {params})
+    //   .subscribe( // manual subscription to observable
+    //     courses => this.courses = courses
+    //   );
+
+    // SECOND METHOD: GET request - using async pipe
+    // this.courses$ = this.http.get<Course[]>('/api/courses', {params})
+  
+    // THIRD METHOD: GET request - dependency injection
+    this.courses$ = this.coursesService.loadCourses();
+  
   }
 
-
+  save(course:Course) {
+    this.coursesService.saveCourse(course)
+      .subscribe(
+        () => console.log('Course saved!')
+      );
+  }
 
 }
